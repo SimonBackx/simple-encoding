84bd5dbc0e3dfcf26259dd9f9d6dfaf633c70412
Simon Backx <hi@simonbackx.com>
2020-05-19 15:52:14 +0200

    Simple patchContainsChanges

--- src/classes/Patchable.ts
+++ src/classes/Patchable.ts
@@ -1,7 +1,14 @@
+import { Encodeable } from "./Encodeable";
+
 export interface Patchable<T, P> {
     patch(patch: T): P;
 }
 
 export function isPatchable<T>(object: T): object is T & Patchable<any, T> {
     return !!(object as any).patch;
+}
+
+export function patchContainsChanges<A, B extends Encodeable & Patchable<A, B>>(patch: A, model: B): boolean {
+    const patched = model.patch(patch);
+    return JSON.stringify(patched.encode()) != JSON.stringify(model.encode());
 }

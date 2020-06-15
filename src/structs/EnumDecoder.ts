f51e59422a0650b1fd86fa398bb39fa318ea5062
Simon Backx <hi@simonbackx.com>
2020-06-15 13:11:21 +0200

    Added support for number enums

--- src/structs/EnumDecoder.ts
+++ src/structs/EnumDecoder.ts
@@ -10,15 +10,31 @@ export class EnumDecoder<E extends { [key: number]: string | number }> implement
     }
 
     decode(data: Data): E[keyof E] {
-        const str = data.string;
-        if (Object.values(this.enum).includes(str)) {
-            return str as E[keyof E];
+        let str: number | string;
+        try {
+            str = data.string;
+            if (Object.values(this.enum).includes(str)) {
+                return str as E[keyof E];
+            }
+        } catch (e) {
+            try {
+                str = data.number;
+                if (Object.values(this.enum).includes(str)) {
+                    return str as E[keyof E];
+                }
+            } catch (e2) {
+                throw new DecodingError({
+                    code: "invalid_field",
+                    message: `Expected a number or string for enum: ` + Object.values(this.enum).join(", "),
+                    field: data.currentField,
+                });
+            }
         }
+
         throw new DecodingError({
             code: "invalid_field",
             message: "Unknown enum value " + str + " expected " + Object.values(this.enum).join(", "),
-            field: data.currentField
-        })
+            field: data.currentField,
+        });
     }
 }
-

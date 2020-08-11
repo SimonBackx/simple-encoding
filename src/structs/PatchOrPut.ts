0b00c65305bd913c4ebcf74e3ae28c5a564449a1
Simon Backx <hi@simonbackx.com>
2020-08-11 20:12:23 +0200

    Implemented patchable on patch or put

--- src/structs/PatchOrPut.ts
+++ src/structs/PatchOrPut.ts
@@ -4,7 +4,7 @@ import { Encodeable } from '../classes/Encodeable';
 import { EncodeContext } from '../classes/EncodeContext';
 import { Patchable } from '../classes/Patchable';
 
-export class PatchOrPut<Put extends Patchable<Patch> & Encodeable, Patch extends Encodeable> implements Encodeable {
+export class PatchOrPut<Put extends Patchable<Patch> & Encodeable, Patch extends Patchable<Patch> & Encodeable> implements Encodeable, Patchable<PatchOrPut<Put, Patch>> {
     value: Patch | Put
     isPut = false
 
@@ -17,6 +17,14 @@ export class PatchOrPut<Put extends Patchable<Patch> & Encodeable, Patch extends
         this.isPut = isPut
     }
 
+    patch(patch: PatchOrPut<Put, Patch>): this {
+        if (patch.isPut) {
+            return patch as any
+        }
+
+        return this.value.patch(patch.getPatch()!) as any
+    }
+
     encode(context: EncodeContext) {
         if (this.isPut) {
             return {
@@ -27,40 +35,40 @@ export class PatchOrPut<Put extends Patchable<Patch> & Encodeable, Patch extends
         return this.value.encode(context)
     }
 
-    static patch<Put extends Patchable<Patch> & Encodeable, Patch extends Encodeable>(value: Patch) {
+    static patch<Put extends Patchable<Patch> & Encodeable, Patch extends Patchable<Patch> & Encodeable>(value: Patch) {
         return new this<Put, Patch>(value, false)
     }
 
-    static put<Put extends Patchable<Patch> & Encodeable, Patch extends Encodeable>(value: Put) {
+    static put<Put extends Patchable<Patch> & Encodeable, Patch extends Patchable<Patch> & Encodeable>(value: Put) {
         return new this<Put, Patch>(value, true)
     }
 
-    get patch(): Patch | undefined {
+    getPatch(): Patch | undefined {
         if (!this.isPatch) {
             return undefined
         }
         return this.value as Patch
     }
 
-    get put(): Put | undefined {
+    getPut(): Put | undefined {
         if (!this.isPut) {
             return undefined
         }
         return this.value as Put
     }
 
-    static apply<Put extends Patchable<Patch> & Encodeable, Patch extends Encodeable>(value: Put, patchOrPut: PatchOrPut<Put, Patch> | Patch): Put {
+    static apply<Put extends Patchable<Patch> & Encodeable, Patch extends Patchable<Patch> & Encodeable>(value: Put, patchOrPut: PatchOrPut<Put, Patch> | Patch): Put {
         if (patchOrPut instanceof PatchOrPut) {
             if (patchOrPut.isPut) {
                 return patchOrPut.value as Put
             }
-            return value.patch(patchOrPut.patch!)
+            return value.patch(patchOrPut.getPatch()!)
         }
         return value.patch(patchOrPut)
     }
 }
 
-export class PatchOrPutDecoder<Put extends Patchable<Patch> & Encodeable, Patch extends Encodeable> implements Decoder<PatchOrPut<Put, Patch>> {
+export class PatchOrPutDecoder<Put extends Patchable<Patch> & Encodeable, Patch extends Patchable<Patch> & Encodeable> implements Decoder<PatchOrPut<Put, Patch>> {
     putDecoder: Decoder<Put>;
     patchDecoder: Decoder<Patch>;
 

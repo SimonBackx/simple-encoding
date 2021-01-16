8eb69ebe441c5c2ed1c95ddefdba363622fd47d6
Simon Backx <hi@simonbackx.com>
2021-01-16 16:35:33 +0100

    Added MapDecoder

---
+++ src/structs/MapDecoder.ts
@@ -0,0 +1,33 @@
+import { SimpleError } from "@simonbackx/simple-errors";
+
+import { Data } from "../classes/Data";
+import { Decoder } from "../classes/Decoder";
+import { ObjectData } from "../classes/ObjectData";
+
+export class MapDecoder<A, B> implements Decoder<Map<A, B>> {
+    keyDecoder: Decoder<A>;
+    valueDecoder: Decoder<B>;
+
+    constructor(keyDecoder: Decoder<A>, valueDecoder: Decoder<B>) {
+        this.keyDecoder = keyDecoder
+        this.valueDecoder = valueDecoder
+    }
+
+    decode(data: Data): Map<A, B> {
+        if (typeof data.value === 'object' && data.value !== null) {
+            const map = new Map<A, B>()
+            for (const key in data.value) {
+                const keyDecoded = new ObjectData(key, data.context, data.addToCurrentField(key)).decode(this.keyDecoder)
+                const valueDecoded = data.field(key).decode(this.valueDecoder)
+                map.set(keyDecoded, valueDecoded)
+            }
+            return map
+        }
+
+        throw new SimpleError({
+            code: "invalid_field",
+            message: `Expected an object at ${data.currentField}`,
+            field: data.currentField,
+        });
+    }
+}

be7811cf16d24e6d93e287926abc3e417f911d7b
Simon Backx <hi@simonbackx.com>
2021-09-23 11:11:50 +0200

    Corrected missing entries

--- src/classes/AutoEncoder.ts
+++ src/classes/AutoEncoder.ts
@@ -295,7 +295,7 @@ export class AutoEncoder implements Encodeable {
                     instance[prop] = this[prop].slice();
 
                     // Make sure we do a deep clone of arrays too
-                    for (const [itemIndex, item] of instance[prop]) {
+                    for (const [itemIndex, item] of (instance[prop] as Array<any>).entries()) {
                         if (isPatchable(item)) {
                             instance[prop][itemIndex] = item.patch({})
                         }

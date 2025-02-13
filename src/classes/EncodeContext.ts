04171fd48bec3fd683338718f116f31ba76e7475
Simon Backx <simon@stamhoofd.be>
2025-02-13 12:05:47 +0100

    Added encode medium

--- src/classes/EncodeContext.ts
+++ src/classes/EncodeContext.ts
@@ -1,4 +1,22 @@
+export enum EncodeMedium {
+    /**
+     * The object will be sent over the network.
+     */
+    Network = 'Network',
+
+    /**
+     * The object will be stored in the database.
+     */
+    Database = 'Database',
+}
+
 export interface EncodeContext {
     version: number;
-    references?: Map<any, Map<string|number, any>>; // class type -> id -> decoded object
+    references?: Map<any, Map<string | number, any>>; // class type -> id -> decoded object
+
+    /**
+     * When encoding, this is the storage type we encode to.
+     * When deconding, this is the storage type we decode from.
+     */
+    medium?: EncodeMedium;
 }
